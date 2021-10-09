99a0f25b21cdb161ed1ab046ab0bcfdb036dc5a1
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-10-10 00:44:01 +0200

    Removing Storyboard

--- Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
+++ Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
@@ -4,7 +4,7 @@ class ExercisesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
     }
 }
 
