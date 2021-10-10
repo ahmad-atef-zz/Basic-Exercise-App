46f370bab32846dc20df8fdf16cbbbc447188069
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-10-10 02:02:28 +0200

    Introducing testing data

---
+++ Basic Exercise AppTests/Exercise+TestData.swift
@@ -0,0 +1,15 @@
+@testable import Basic_Exercise_App
+extension Exercise {
+    static let moveHip = Exercise(
+        id: 1,
+        name: "Move hip",
+        coverImageUrl: "https://d32oopmphic0po.cloudfront.net/v1/images/body/en-US/body-exercise-4-14.png",
+        videoUrl: "https://d32oopmphic0po.cloudfront.net/v1/videos/body/en-US/body-exercise-4-14.mp4")
+
+    static let sidePlank = Exercise(
+        id: 4,
+        name: "Side Plank",
+        coverImageUrl: "https://d32oopmphic0po.cloudfront.net/v1/images/body/en-US/body-exercise-5-5.png",
+        videoUrl: "https://d32oopmphic0po.cloudfront.net/v1/videos/body/en-US/body-exercise-5-5.mp4")
+
+}
