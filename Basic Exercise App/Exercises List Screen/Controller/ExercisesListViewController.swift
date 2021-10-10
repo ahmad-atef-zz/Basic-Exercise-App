68cc0aab03adbe37b1d5235be065fcbc6f7c544c
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-10-10 23:34:06 +0200

    Inject DataLoader into ExercisesListViewController

--- Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
+++ Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
@@ -1,10 +1,24 @@
 import UIKit
+import Combine
 
 class ExercisesListViewController: UIViewController {
+
+    private let dataSource: ExerciseDataSource
+    private var cancellable: Set<AnyCancellable> = []
+
+    init(_ dataSource: ExerciseDataSource = ExerciseDataLoader()) {
+        self.dataSource = dataSource
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        self.title = "Exercise Overview 🏋️"
+        dataSource.loadData()
     }
-}
 
