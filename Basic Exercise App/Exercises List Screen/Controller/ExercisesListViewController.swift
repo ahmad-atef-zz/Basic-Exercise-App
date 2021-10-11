604dec8e10b5fada3678ee6c880aaeed65375462
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-10-11 23:39:18 +0200

    Adding dataLoader into controller

--- Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
+++ Basic Exercise App/Exercises List Screen/Controller/ExercisesListViewController.swift
@@ -1,24 +1,47 @@
 import UIKit
 import Combine
 
-class ExercisesListViewController: UIViewController {
+final class ExercisesListViewController: UIViewController {
 
-    private let dataSource: ExerciseDataSource
+    private let dataLoader: ExerciseDataLoader = ExerciseDataLoader()
     private var cancellable: Set<AnyCancellable> = []
-
-    init(_ dataSource: ExerciseDataSource = ExerciseDataLoader()) {
-        self.dataSource = dataSource
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private var collectionView: UICollectionView!
+    private var loadingIndicator: UIActivityIndicatorView {
+        let loadingIndicator = UIActivityIndicatorView(style: .medium)
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.startAnimating()
+        return loadingIndicator
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.view.backgroundColor = .systemBackground
         self.title = "Exercise Overview 🏋️"
-        dataSource.loadData()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: loadingIndicator)
+
+        fetchData()
     }
 
+    private func fetchData() {
+        dataLoader.$isLoading
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] isLoading in
+                if isLoading {
+                    self?.loadingIndicator.startAnimating()
+                } else {
+                    self?.loadingIndicator.stopAnimating()
+                }
+            }
+            .store(in: &cancellable)
+
+        dataLoader.dataChanged
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.collectionView.reloadData()
+            }
+            .store(in: &cancellable)
+
+        dataLoader.loadData()
+    }
+}
