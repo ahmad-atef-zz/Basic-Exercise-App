ecb9cefbcd0cf94bc105e95fdb1f8c8ccd8d557b
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2021-10-13 15:55:51 +0200

    Adding initial Setup

---
+++ Basic Exercise App/Training session/Behaviours/TrainingSessionBehaviours.swift
@@ -0,0 +1,8 @@
+//
+//  TrainingSessionBehaviours.swift
+//  Basic Exercise App
+//
+//  Created by Ahmed Atef Ali Ahmed on 13.10.21.
+//
+
+import Foundation
---
+++ Basic Exercise App/Training session/Data Loader/TrainingSessionDataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  TrainingSessionDataLoader.swift
+//  Basic Exercise App
+//
+//  Created by Ahmed Atef Ali Ahmed on 13.10.21.
+//
+
+import Foundation
